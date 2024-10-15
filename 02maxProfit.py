eaa0a3060680c3fc8d2859938132413da4953eca
Najia Jahan <90651061+najia096@users.noreply.github.com>
2024-10-15 10:27:03 -0400

    Create 02maxProfit.py

---
+++ 02maxProfit.py
@@ -0,0 +1,25 @@
+'''
+#121. Best Time to Buy and Sell Stock
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+'''
+'''
+Time complexity: O(n)
+Space complexity: O(1)
+'''
+
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                profit = price - min_price
+                if profit > max_profit:
+                    max_profit = profit
+        return max_profit
